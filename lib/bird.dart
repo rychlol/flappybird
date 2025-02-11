ceec82b4a101336404992d221109e09199a54f74
rychlol <rjhc_azul@outlook.es>
2025-02-11 17:03:18 -0600

    mi_primer_commit

---
+++ lib/bird.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyBird extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 90,
+      width: 90,
+        child: Image.asset(
+          'lib/images/Flappy.png'
+        ),
+      );
+  }
+}
---
+++ lib/homepage.dart
@@ -0,0 +1,111 @@
+import 'dart:async';
+
+import 'package:flappybird/bird.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  static double birdYaxis = 0;
+  double time = 0;
+  double height = 0;
+  double initialHeight = birdYaxis;
+  bool gameHasStarted = false;
+
+  void jump() {
+    setState(() {
+      time = 0;
+      initialHeight = birdYaxis;
+    });
+  }
+
+  void startGame() {
+    gameHasStarted = true;
+    Timer.periodic(Duration(milliseconds: 60), (timer) {
+      time += 0.04;
+      height = -4.9 * time * time + 2.8 * time;
+      setState(() {
+        birdYaxis = initialHeight - height;
+      });
+      if (birdYaxis > 1) {
+        timer.cancel();
+        gameHasStarted = false;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            flex: 2,
+            child: Stack(
+              children: [
+                GestureDetector(
+                onTap: () {
+                  if (gameHasStarted) {
+                    jump();
+                  } else {
+                    startGame();
+                  }
+                },
+                child: AnimatedContainer(
+                  alignment: Alignment(0, birdYaxis),
+                  duration: Duration(milliseconds: 0),
+                  color: Colors.blue,
+                  child: MyBird(),
+                )),
+                Container(
+                  child: Text("TAP tO PLAY"),
+                )
+              ],
+            )
+          ),
+          Container(
+            height: 15,
+            color: Colors.green,
+          ),
+          Expanded(
+            child: Container(
+              color: Colors.brown,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("SCORE",
+                          style: TextStyle(color: Colors.white, fontSize: 20)),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Text("0",
+                          style: TextStyle(color: Colors.white, fontSize: 35)),
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("BEST",
+                          style: TextStyle(color: Colors.white, fontSize: 20)),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Text("10",
+                          style: TextStyle(color: Colors.white, fontSize: 35)),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
